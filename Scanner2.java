e578222c8599458ee4c7f465994dd72cf0a6d47b
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-03-10 09:50:14 +0530

    Input Using Scanner Class

---
+++ Scanner2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Scanner2{
+        public static void main(String[] args){
+                Scanner Sc=new Scanner(System.in);
+                String Name;
+                System.out.println("Enter the name : ");
+                Name=Sc.nextLine();
+                System.out.println("Name = "+Name);
+        }
+
+}
