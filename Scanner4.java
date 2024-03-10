647f96f0ff2e4f5e04033d26e164ab3c898e695f
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-03-10 09:53:29 +0530

    Input Using Scanner Class

---
+++ Scanner4.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Scanner4{
+        public static void main(String[] args){
+                Scanner Sc=new Scanner(System.in);
+                Double Salary;
+                System.out.println("Enter the  Salary : ");
+                Salary=Sc.nextDouble();
+                System.out.println("Salary = "+Salary);
+        }
+
+}
