8f0cca35add13e1bc5cab93bd93c46d5ae468e74
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-03-10 09:49:28 +0530

    Input Using Scanner Class

---
+++ Scanner1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Scanner1{
+        public static void main(String[] args){
+                Scanner Sc=new Scanner(System.in);
+                int Age;
+                System.out.println("Enter the age : ");
+                Age=Sc.nextInt();
+                System.out.println("Age = "+Age);
+        }
+
+}
