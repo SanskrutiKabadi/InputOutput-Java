daf101cbf50af8486510ae93293b95143be4d118
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-03-10 09:50:38 +0530

    Input Using Scanner Class

---
+++ Scanner3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Scanner3{
+        public static void main(String[] args){
+                Scanner Sc=new Scanner(System.in);
+                Float Temprature;
+                System.out.println("Enter the temprature : ");
+                Temprature=Sc.nextFloat();
+                System.out.println("Temprature = "+Temprature);
+        }
+
+}
