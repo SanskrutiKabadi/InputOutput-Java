607c6b7ec76ba2cb9a561336bbaa5b16f166f183
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-03-11 08:29:39 +0530

    Input Using Scanner Class

---
+++ Scanner6.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Scanner6{
+        public static void main(String[] args){
+                Scanner Sc=new Scanner(System.in);
+                char Division;
+                System.out.println("Enter the  division : ");
+                Division=Sc.next().charAt(0);
+                System.out.println("Division = "+Division);
+        }
+
+}
