27f427f7f3473ee7af556c4973ca34c149659dfe
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-03-11 08:28:14 +0530

    Java Input using Scanner

---
+++ Scanner5.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Scanner5{
+        public static void main(String[] args){
+                Scanner Sc=new Scanner(System.in);
+                long Number;
+                System.out.println("Enter the  no : ");
+                Number=Sc.nextLong();
+                System.out.println("Number = "+Number);
+        }
+
+}
