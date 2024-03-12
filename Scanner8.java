d52bbee0c1749335bac97c62fb503284bbf397e4
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-03-12 07:49:01 +0530

    Input Using Scanner Class

---
+++ Scanner8.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Scanner8{
+        public static void main(String[] args){
+                Scanner Sc=new Scanner(System.in);
+                short Number;
+                System.out.println("Enter the  number : ");
+                Number=Sc.nextShort();
+                System.out.println("Number = "+Number);
+        }
+
+}
