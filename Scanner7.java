d993434efcf1d6a6929c8699c9945357a52bd1b6
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-03-12 07:48:31 +0530

    Input Using Scanner Class

---
+++ Scanner7.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Scanner7{
+        public static void main(String[] args){
+                Scanner Sc=new Scanner(System.in);
+                Byte Number;
+                System.out.println("Enter the  number : ");
+                Number=Sc.nextByte();
+                System.out.println("Number = "+Number);
+        }
+
+}
