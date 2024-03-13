1bcb14eb6de45ac95944539bab41af27775b284e
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-03-13 08:19:37 +0530

    Input Using Scanner Class

---
+++ Scanner9.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Scanner9{
+        public static void main(String[] args){
+                Scanner Sc=new Scanner(System.in);
+                boolean Answer;
+                System.out.println("Enter the  Answer : ");
+                Answer=Sc.nextBoolean();
+                System.out.println("Answer = "+Answer);
+        }
+
+}
